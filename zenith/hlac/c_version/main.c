06196efe847ad2ce32da948ce0093074ea91b608
ZakiarV <alexandrefilippone@gmail.com>
2026-06-02 16:41:09 -0400

    Continued C version implementation,

--- zenith/hlac/c_version/main.c
+++ zenith/hlac/c_version/main.c
@@ -156,33 +156,63 @@ End of system call implementations.
 Start 
 */
 
+
+char *register_names[] = {
+    "rax", "rbx", "rcx", "rdx", "rsi", "rdi", "rsp", "rbp", // 8
+    "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15", // 8
+    "eax", "ebx", "ecx", "edx", "esi", "edi", "esp", "ebp", // 8
+    "r8d", "r9d", "r10d", "r11d", "r12d", "r13d", "r14d", "r15d", // 8
+    "ax",  "bx",  "cx",  "dx",  "si",  "di",  "sp",  "bp",  // 8
+    "r8w", "r9w", "r10w", "r11w", "r12w", "r13w", "r14w", "r15w", // 8
+    "al",  "bl",  "cl",  "dl",  "sil", "dil", "spl", "bpl", // 8
+    "r8b", "r9b", "r10b", "r11b", "r12b", "r13b", "r14b", "r15b" // 8
+    "ah",  "bh",  "ch",  "dh" // 4
+};
+
+i64 num_registers = sizeof(register_names) / sizeof(register_names[0]);
+
+char *instruction_names[] = {
+    "mov", "add", "sub", "mul", "div", "mod", "and", "or", "xor", "not",
+    "shl", "shr", "sar", "rol", "ror",
+    "cmp", "test",
+    "jmp", "je", "jne", "jg", "jge", "jl", "jle", "jz", "jnz",
+    "syscall"
+    // ... (more instructions can be added as needed)
+};
+
+i64 num_instructions = sizeof(instruction_names) / sizeof(instruction_names[0]);
+
 enum {
     O_RDONLY = 0,
     O_WRONLY = 1,
     O_RDWR   = 2,
-    O_CREAT  = 64,
-    O_EXCL   = 128,
-    O_NOCTTY = 256,
-    O_TRUNC  = 512,
-    O_APPEND  = 1024,
-    O_DIRECTORY = 65536,
-    O_NOFOLLOW = 131072,
-    O_CLOEXEC  = 524288
+    O_CREAT  = 64,      // 0100
+    O_EXCL   = 128,     // 0200
+    O_NOCTTY = 256,     // 0400
+    O_TRUNC  = 512,     // 01000
+    O_APPEND = 1024,    // 02000
+
+    O_DIRECTORY = 65536,  // 0200000
+    O_NOFOLLOW  = 131072, // 0400000
+    O_CLOEXEC   = 524288  // 02000000
 };
 
 enum {
-    M_OTHER_EXEC  = 1,    // 0o1
-    M_OTHER_WRITE = 2,    // 0o2
-    M_OTHER_READ  = 4,    // 0o4
-    M_GROUP_EXEC  = 8,    // 0o10
-    M_GROUP_WRITE = 16,   // 0o20
-    M_GROUP_READ  = 32,   // 0o40
-    M_OWNER_EXEC   = 64,  // 0o100
-    M_OWNER_WRITE  = 128, // 0o200
-    M_OWNER_READ   = 256, // 0o400
-    M_STICKY      = 512,  // 0o1000
-    M_SETGID      = 1024, // 0o2000
-    M_SETUID      = 2048  // 0o4000
+    M_OTHER_EXEC  = 1,     // 0001
+    M_OTHER_WRITE = 2,     // 0002
+    M_OTHER_READ  = 4,     // 0004
+
+    M_GROUP_EXEC  = 8,     // 0010
+    M_GROUP_WRITE = 16,    // 0020
+    M_GROUP_READ  = 32,    // 0040
+
+    M_OWNER_EXEC  = 64,    // 0100
+    M_OWNER_WRITE = 128,   // 0200
+    M_OWNER_READ  = 256,   // 0400
+
+    M_STICKY      = 512,   // 01000
+    M_SETGID      = 1024,  // 02000
+    M_SETUID      = 2048   // 04000
 };
 
 enum {
@@ -200,17 +230,6 @@ enum {
 };
 
 
-typedef enum TokenType {
-    TOKEN_FUNCTION,
-    TOKEN_END,
-    TOKEN_LOCALS,
-    TOKEN_ENDLOCALS,
-    TOKEN_DATA,
-    TOKEN_ENDDATA,
-    TOKEN_LABEL,
-} TokenType;
-
-
 typedef struct String {
     char *data;
     i64 length;
@@ -223,15 +242,10 @@ typedef struct ListString {
 } ListString;
 
 
-typedef struct Token {
-    String value;
-    TokenType type;
-} Token;
-
-
 typedef struct File {
     char *content;
     i64 size;
+    i64 descriptor;
 } File;
 
 
@@ -272,6 +286,16 @@ bool string_equals(const char *s1, const char *s2) {
 }
 
 
+bool is_string_in_array(const char *s, char **array, i64 array_length) {
+    for (i64 i = 0; i < array_length; i++) {
+        if (string_equals(s, array[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 i64 open_file(const char *pathname, i64 flags, i64 mode) {
     return sys_openat(-100 /* AT_FDCWD */, pathname, flags, mode);
 }
@@ -284,10 +308,25 @@ File read_file(const char *path) {
     i64 file_size = statbuf.st_size;
     char *file_content = (char *)sys_mmap(0, file_size, (i64)(PROT_READ|PROT_WRITE), (i64)(MAP_PRIVATE|MAP_ANONYMOUS), -1, 0);
     sys_read(fd, file_content, file_size);
-    sys_close(fd);
     file.content = file_content;
     file.size = file_size;
+    file.descriptor = fd;
     return file;
+}
+
+
+File create_file(const char *path, i64 flags, i64 mode) {
+    File file;
+    i64 fd = open_file(path, flags, mode);
+    file.content = 0;
+    file.size = 0;
+    file.descriptor = fd;
+    return file;
+}
+
+
+i64 write_file(File file, const char *content, i64 size) {
+    return sys_write(file.descriptor, content, size);
 }
 
 
@@ -388,28 +427,339 @@ ListString sanitize_list_string(ListString list) {
 }
 
 
+char *int_cstring(i64 integer) {
+    char buffer[21];
+    i8 index = 0;
+    while (integer > 0) {
+        buffer[index++] = '0' + (integer % 10);
+        integer /= 10;
+    }
+    // Reverse the buffer to get the correct order.
+    char temp[21];
+    for (i8 i = 0; i < index; i++) {
+        temp[i] = buffer[index - i - 1];
+    }
+    temp[index] = '\0';
+    return string_duplicate(temp);
+}
+
+
+i64 cstring_int(const char *s) {
+    if (get_strlen(s) == 0) {
+        return 0;
+    }
+    if (get_strlen(s) > 20) {
+        return 0; // Too long to be a valid integer
+    }
+    i64 result = 0;
+    for (i64 i = 0; s[i] != '\0'; i++) {
+        if (s[i] < '0' || s[i] > '9') {
+            return 0; // Invalid character
+        }
+        result = result * 10 + (s[i] - '0');
+    }
+    return result;
+}
+
+
+bool is_cstring_integer(const char *s) {
+    if (get_strlen(s) == 0) {
+        return false;
+    }
+    for (i64 i = 0; s[i] != '\0'; i++) {
+        if (s[i] < '0' || s[i] > '9') {
+            return false; // Invalid character
+        }
+    }
+    return true;
+}
+
+
+void print_integer(i64 integer) {
+    char *cstring = int_cstring(integer);
+    i64 length = get_strlen(cstring);
+    sys_write(1, cstring, length);
+    sys_munmap(cstring, length + 1);
+}
+
+
+void print_char_integer(char character) {
+    i64 char_ = (i64)character;
+    print_integer(char_);
+    sys_write(1, "\n", 1);
+}
+
+
+void free_string(String s) {
+    sys_munmap(s.data, s.length + 1);
+}
+
+
+void free_list_string(ListString list) {
+    for (i64 i = 0; i < list.count; i++) {
+        free_string(list.items[i]);
+    }
+    sys_munmap(list.items, list.count * sizeof(String));
+}
+
+
+void free_file(File file) {
+    sys_munmap(file.content, file.size + 1);
+    sys_close(file.descriptor);
+}
+
+
+void free_cstring(char *cstring) {
+    i64 length = get_strlen(cstring);
+    sys_munmap(cstring, length + 1);
+}
+
+
+char *string_uppercase(const char *s) {
+    i64 len = get_strlen(s);
+    char *upper = (char *)sys_mmap(0, len + 1, (i64)(PROT_READ|PROT_WRITE), (i64)(MAP_PRIVATE|MAP_ANONYMOUS), -1, 0);
+    for (i64 i = 0; i < len; i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            upper[i] = s[i] - ('a' - 'A');
+        } else {
+            upper[i] = s[i];
+        }
+    }
+    upper[len] = '\0';
+    return upper;
+}
+
+
+char *string_lowercase(const char *s) {
+    i64 len = get_strlen(s);
+    char *lower = (char *)sys_mmap(0, len + 1, (i64)(PROT_READ|PROT_WRITE), (i64)(MAP_PRIVATE|MAP_ANONYMOUS), -1, 0);
+    for (i64 i = 0; i < len; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            lower[i] = s[i] + ('a' - 'A');
+        } else {
+            lower[i] = s[i];
+        }
+    }
+    lower[len] = '\0';
+    return lower;
+}
+
+
+typedef enum TokenType {
+    TOKEN_REGISTER,
+    TOKEN_INSTRUCTION,
+    TOKEN_COLON,
+    TOKEN_SEMICOLON,
+    TOKEN_SYMBOL,
+    TOKEN_MEMORY_OPERAND,
+    TOKEN_LBRACKET,
+    TOKEN_RBRACKET,
+    TOKEN_LABEL,
+    TOKEN_FUNCTION,
+    TOKEN_END,
+    TOKEN_INTEGER,
+    TOKEN_LOCALS,
+    TOKEN_ENDLOCALS,
+    TOKEN_TEXT,
+    TOKEN_DATA,
+    TOKEN_ENDTEXT,
+    TOKEN_ENDDATA,
+    TOKEN_SIZE,
+    TOKEN_WHERE,
+    TOKEN_STACK,
+    TOKEN_REGISTER_LOC,
+    TOKEN_QWORD,
+    TOKEN_DWORD,
+    TOKEN_WORD,
+    TOKEN_BYTE,
+    TOKEN_EXTERN,
+    TOKEN_EXPORT,
+    TOKEN_SAVE_REGS,
+    TOKEN_RESTORE_REGS,
+    TOKEN_DB,
+    TOKEN_DW,
+    TOKEN_DD,
+    TOKEN_DQ,
+     // ... (more token types can be added as needed)
+} TokenType;
+
+
+typedef struct Token {
+    TokenType type;
+    String value;
+} Token;
+
+
+typedef struct TokenList {
+    Token *items;
+    i64 count;
+} TokenList;
+
+
+void free_token(Token token) {
+    free_string(token.value);
+}
+
+
+void free_token_list(TokenList list) {
+    for (i64 i = 0; i < list.count; i++) {
+        free_token(list.items[i]);
+    }
+    sys_munmap(list.items, list.count * sizeof(Token));
+}
+
+
+void print_token(Token token) {
+    char *type_str;
+    switch (token.type) {
+        case TOKEN_REGISTER: type_str = "REGISTER"; break;
+        case TOKEN_INSTRUCTION: type_str = "INSTRUCTION"; break;
+        case TOKEN_COLON: type_str = "COLON"; break;
+        case TOKEN_SEMICOLON: type_str = "SEMICOLON"; break;
+        case TOKEN_SYMBOL: type_str = "SYMBOL"; break;
+        case TOKEN_MEMORY_OPERAND: type_str = "MEMORY_OPERAND"; break;
+        case TOKEN_LBRACKET: type_str = "LBRACKET"; break;
+        case TOKEN_RBRACKET: type_str = "RBRACKET"; break;
+        case TOKEN_LABEL: type_str = "LABEL"; break;
+        case TOKEN_FUNCTION: type_str = "FUNCTION"; break;
+        case TOKEN_END: type_str = "END"; break;
+        case TOKEN_INTEGER: type_str = "INTEGER"; break;
+        case TOKEN_LOCALS: type_str = "LOCALS"; break;
+        case TOKEN_ENDLOCALS: type_str = "ENDLOCALS"; break;
+        case TOKEN_TEXT: type_str = "TEXT"; break;
+        case TOKEN_DATA: type_str = "DATA"; break;
+        case TOKEN_ENDTEXT: type_str = "ENDTEXT"; break;
+        case TOKEN_ENDDATA: type_str = "ENDDATA"; break;
+        case TOKEN_SIZE: type_str = "SIZE"; break;
+        case TOKEN_WHERE: type_str = "WHERE"; break;
+        case TOKEN_STACK: type_str = "STACK"; break;
+        case TOKEN_REGISTER_LOC: type_str = "REGISTER_LOC"; break;
+        case TOKEN_QWORD: type_str = "QWORD"; break;
+        case TOKEN_DWORD: type_str = "DWORD"; break;
+        case TOKEN_WORD: type_str = "WORD"; break;
+        case TOKEN_BYTE: type_str = "BYTE"; break;
+        case TOKEN_EXTERN: type_str = "EXTERN"; break;
+        case TOKEN_EXPORT: type_str = "EXPORT"; break;
+        case TOKEN_SAVE_REGS: type_str = "SAVE_REGS"; break;
+        case TOKEN_RESTORE_REGS: type_str = "RESTORE_REGS"; break;
+        case TOKEN_DB: type_str = "DB"; break;
+        case TOKEN_DW: type_str = "DW"; break;
+        case TOKEN_DD: type_str = "DD"; break;
+        case TOKEN_DQ: type_str = "DQ"; break;
+        default: type_str = "UNKNOWN";
+    }
+    sys_write(1, "[", 1);
+    sys_write(1, type_str, get_strlen(type_str));
+    sys_write(1, ": ", 2);
+    sys_write(1, token.value.data, token.value.length);
+    sys_write(1, "]\n", 2);
+}
+
+
+TokenList tokenize_file(File file) {
+    char *cstring = file_content_to_cstring(file);
+    ListString unsanitized_strings = split_cstring(cstring, " ,\n\r\t");
+    ListString strings = sanitize_list_string(unsanitized_strings);
+    free_list_string(unsanitized_strings);
+    free_cstring(cstring);
+
+    Token *tokens = (Token *)sys_mmap(0, strings.count * sizeof(Token), (i64)(PROT_READ|PROT_WRITE), (i64)(MAP_PRIVATE|MAP_ANONYMOUS), -1, 0);
+    for (i64 i = 0; i < strings.count; i++) {
+        Token token;
+        if (is_string_in_array(strings.items[i].data, register_names, num_registers)) {
+            token.type = TOKEN_REGISTER;
+        } else if (is_string_in_array(strings.items[i].data, instruction_names, num_instructions)) {
+            token.type = TOKEN_INSTRUCTION;
+        } else if (string_equals(strings.items[i].data, ":")) {
+            token.type = TOKEN_COLON;
+        } else if (string_equals(strings.items[i].data, ";")) {
+            token.type = TOKEN_SEMICOLON;
+        } else if (string_equals(strings.items[i].data, "[")) {
+            token.type = TOKEN_LBRACKET;
+        } else if (string_equals(strings.items[i].data, "]")) {
+            token.type = TOKEN_RBRACKET;
+        } else if (is_cstring_integer(strings.items[i].data)) {
+            token.type = TOKEN_INTEGER;
+        } else if (string_equals(strings.items[i].data, "extern")) {
+            token.type = TOKEN_EXTERN;
+        } else if (string_equals(strings.items[i].data, "export")) {
+            token.type = TOKEN_EXPORT;
+        } else if (string_equals(strings.items[i].data, "locals")) {
+            token.type = TOKEN_LOCALS;
+        } else if (string_equals(strings.items[i].data, "endlocals")) {
+            token.type = TOKEN_ENDLOCALS;
+        } else if (string_equals(strings.items[i].data, "TEXT")) {
+            token.type = TOKEN_TEXT;
+        } else if (string_equals(strings.items[i].data, "DATA")) {
+            token.type = TOKEN_DATA;
+        } else if (string_equals(strings.items[i].data, "ENDTEXT")) {
+            token.type = TOKEN_ENDTEXT;
+        } else if (string_equals(strings.items[i].data, "ENDDATA")) {
+            token.type = TOKEN_ENDDATA;
+        } else if (string_equals(strings.items[i].data, "size")) {
+            token.type = TOKEN_SIZE;
+        } else if (string_equals(strings.items[i].data, "where")) {
+            token.type = TOKEN_WHERE;
+        } else if (string_equals(strings.items[i].data, "stack")) {
+            token.type = TOKEN_STACK;
+        } else if (string_equals(strings.items[i].data, "register")) {
+            token.type = TOKEN_REGISTER_LOC;
+        } else if (string_equals(string_uppercase(strings.items[i].data), "QWORD")) {
+            token.type = TOKEN_QWORD;
+        } else if (string_equals(string_uppercase(strings.items[i].data), "DWORD")) {
+            token.type = TOKEN_DWORD;
+        } else if (string_equals(string_uppercase(strings.items[i].data), "WORD")) {
+            token.type = TOKEN_WORD;
+        } else if (string_equals(string_uppercase(strings.items[i].data), "BYTE")) {
+            token.type = TOKEN_BYTE;
+        } else if (string_equals(strings.items[i].data, "end")) {
+            token.type = TOKEN_END;
+        } else if (string_equals(strings.items[i].data, "function")) {
+            token.type = TOKEN_FUNCTION;
+        } else if (string_equals(strings.items[i].data, "label")) {
+            token.type = TOKEN_LABEL;
+        } else if (string_equals(strings.items[i].data, "save_registers")) {
+            token.type = TOKEN_SAVE_REGS;
+        } else if (string_equals(strings.items[i].data, "restore_registers")) {
+            token.type = TOKEN_RESTORE_REGS;
+        } else if (string_equals(strings.items[i].data, "db")) {
+            token.type = TOKEN_DB;
+        } else if (string_equals(strings.items[i].data, "dw")) {
+            token.type = TOKEN_DW;
+        } else if (string_equals(strings.items[i].data, "dd")) {
+            token.type = TOKEN_DD;
+        } else if (string_equals(strings.items[i].data, "dq")) {
+            token.type = TOKEN_DQ;
+        } else if (strings.items[i].data[0] == '[' && strings.items[i].data[strings.items[i].length - 1] == ']') {
+            token.type = TOKEN_MEMORY_OPERAND;
+        } else {
+            token.type = TOKEN_SYMBOL; // Default to symbol for now
+        }
+
+        token.value = copy_string(strings.items[i]);
+        tokens[i] = token;
+    }
+
+    TokenList result;
+    result.items = tokens;
+    result.count = strings.count;
+    return result;
+}
+
+
 i64 main(i64 argc, char **argv) {
     File file = read_file(argv[1]);
+    File output_file = create_file(argv[2], (i64)(O_RDWR | O_CREAT | O_TRUNC), (i64)(M_OWNER_READ | M_GROUP_READ | M_OTHER_READ | M_OWNER_WRITE | M_GROUP_WRITE | M_OTHER_WRITE));
 
-    char *cstring = file_content_to_cstring(file);
+    TokenList tokens = tokenize_file(file);
 
-    ListString tokens = split_cstring(cstring, " ,\t\n");
-    ListString sanitized_tokens = sanitize_list_string(tokens);
-    
-    sys_munmap(file.content, file.size);
-    sys_munmap(cstring, get_strlen(cstring) + 1);
-    sys_munmap(tokens.items, tokens.count * sizeof(String));
-
-    for (i64 i = 0; i < sanitized_tokens.count; i++) {
-        sys_write(1, sanitized_tokens.items[i].data, sanitized_tokens.items[i].length);
-        sys_write(1, "\n", 1);
+    for (i64 i = 0; i < tokens.count; i++) {
+        print_token(tokens.items[i]);
     }
 
-    for (i64 i = 0; i < sanitized_tokens.count; i++) {
-        sys_munmap(sanitized_tokens.items[i].data, sanitized_tokens.items[i].length + 1);
-    }
-
-    sys_munmap(sanitized_tokens.items, sanitized_tokens.count * sizeof(String));
+    free_file(file);
+    free_file(output_file);
+    free_token_list(tokens);
     return 0;
 }
 
